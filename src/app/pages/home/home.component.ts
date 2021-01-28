7792230f9999dd155569b834c479ee00b96e361b
Rubens Junior <rubinhochagas@gmail.com>
2021-01-28 11:51:30 -0300

    storage search items

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of, Subject } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { GithubService } from 'src/app/shared/services/github.service';
 
 @Component({
@@ -14,38 +14,40 @@ export class HomeComponent implements OnInit {
   content:any = [];
   page = 1;
   termValue: string;
+  termSearch: any = [];
+  valueTerm: string = '';
 
   constructor(private serviceGithub: GithubService) { }
 
   ngOnInit(): void {
     this.searchRep();
+    this.getTerm();
   }
 
   searchRep(){
-    setTimeout(() => {
-      this.searchTerms.pipe(
-        debounceTime(1000), 
-        distinctUntilChanged(),
-        switchMap(
-          term => 
-            term 
-              ? 
-              this.serviceGithub.searchUser(term, this.page, 12)
-              : 
-              of<any>([])
-        ),
-        catchError(error => {
-          // TODO: real error handling
-          return of<any>([]);
-        })
-      ).subscribe(res => { 
-        this.content = res;
-      });
-    }, 1000);
+    this.searchTerms.pipe(
+      debounceTime(1000), 
+      distinctUntilChanged(),
+      switchMap(
+        term => 
+          term 
+            ? 
+            this.serviceGithub.searchUser(term, this.page, 12)
+            : 
+            of<any>([])
+      ),
+      catchError(error => {
+        // TODO: real error handling
+        return of<any>([]);
+      })
+    ).subscribe(res => { 
+      this.content = res;
+      this.getTerm();
+    });
   }
 
   search(term){
-    this.termValue =  term.target.value
+    this.termValue = term.target.value
     if (this.termValue && this.termValue.trim() !== '') {
       this.searchTerms.next(term.target.value);
     } else {
@@ -57,4 +59,25 @@ export class HomeComponent implements OnInit {
     this.page += e;
     this.serviceGithub.searchUser(this.termValue, this.page, 12).subscribe((res => (this.content = res)))
   }
+
+
+  removeTerm(){
+    localStorage.removeItem('term');
+    this.getTerm();
+  }
+
+  getTerm(){
+    this.termSearch = JSON.parse(localStorage.getItem('term'));
+    // this.termSearch.sort((val)=> {return new Date(val.date)})
+
+    this.termSearch.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+
+  }
+
+  selectChange(event) {
+    this.valueTerm = event.target.value;
+    this.search(event)
+  }
+
 }
--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -7,12 +7,13 @@ import { RequestService } from './generic/request.service';
 })
 export class GithubService {
 
-  
+  termSearch: any = []
 
   constructor(private requestService: RequestService) { }
   
     // response repositories public
     searchUser(search?:string, page?:number, per_page?: number): Observable<any> {
+      this.setTerms(search)
       let params = {
         'q': search ? search : '',
         'page': page ? page : '',
@@ -32,8 +33,25 @@ export class GithubService {
       }
         return this.requestService.get(`/users/${user}/repos`, params)
       }
+    
+    
+    setTerms(term){
+      let terms = JSON.parse(localStorage.getItem('term'));
+      let currentDate = new Date();
 
+      if (terms) {
+        const newTerm = { 'term': term, 'date': currentDate }
+        terms.push(newTerm);
+        localStorage.setItem('term', JSON.stringify(terms));
 
+      }else {
 
+        let a = []
+        const newTerm = { 'term': term, 'date': currentDate }
+        a.push(newTerm);
+
+        localStorage.setItem('term', JSON.stringify(a));
+      }
+    }
 
 }
